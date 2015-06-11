5694667b01356f6687137db2f7a1118375301457
Ryan <ryanbent01@gmail.com>
2015-06-11 03:09:35 +0000

    first commit

---
+++ calculator.rb
@@ -0,0 +1,48 @@
+#User is prompted to input one number
+
+#User puts first number then
+#user 
+
+
+
+def say(msg)
+  puts "=> #{msg}"
+end
+
+say "What's the first number?"
+num1= gets.chomp
+
+say "What's the second number?"
+num2 = gets.chomp
+
+
+
+
+say "1) add 2) subtract 3) multiply 4) divide"
+operator = gets.chomp 
+until ["1", "2", "3", "4"].include?(operator)
+  say "That's a invalid selection"
+  say "Please try again"
+  operator = gets.chomp
+end
+
+
+
+if operator == "1"
+  result = num1.to_i + num2.to_i
+elsif operator == "2"
+  result = num1.to_i - num2.to_i
+elsif operator == "3"
+  result = num1.to_i * num2.to_i
+else
+  result = num1.to_f / num2.to_f
+end
+  
+
+puts "Result is #{result}"
+
+
+
+
+
+
